dd8ae98d27fb4129f1a72efb1c17a1e448663eb4
mvchfvn <mimi123meg@gmail.com>
2018-07-25 16:23:57 +0000

    automatically backed up by learn

--- summer_olympics_hash.rb
+++ summer_olympics_hash.rb
@@ -5,11 +5,11 @@ def create_olympics_hash
   return olympics_hash
 end
 
-def add_a_key_value_pair(hash, key, value)
+def add_a_key_value_pair
   # Implement this method so that it adds a key value pair to the hash created in create_olympics_hash
-  hash[key] = value
-  olympics_hash = hash
-  return olympics_hash
+  hash = create_olympics_hash
+  hash[:Atlanta] = "1996"
+  return hash
 end
 
 def iterate_through_hash
